e492b3d50c7ce601a91487b9882fc563da9bdf61
zerbase <zerbase@naver.com>
2022-04-26 01:32:18 +0900

    add easy leet

--- exam/problems/tree.py
+++ exam/problems/tree.py
@@ -3,14 +3,12 @@ tree_list = list(map(int, input().split()))
 
 max_tree = max(tree_list)
 min_tree = 0
-# set_h = max_tree // 2 
 
 while min_tree <= max_tree:
     mid = (min_tree + max_tree) // 2
     sums = 0
 
     for i in tree_list:
-        #off_tree = i - mid 
         if i >= mid:
             sums += i - mid
     if sums < need_tree:
