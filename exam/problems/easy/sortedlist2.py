c479e21617b5799117e02358a1d348481e57ced9
zerbase <zerbase@naver.com>
2022-05-07 23:45:55 +0900

    fix bug for sortedlist2.py

--- exam/problems/easy/sortedlist2.py
+++ exam/problems/easy/sortedlist2.py
@@ -10,21 +10,17 @@ class Solution(object):
         :rtype: ListNode
         """
         r_list = ListNode(head.val)
-        b_val = None
-        
+
+        r_head = r_list
         while head != None:
-            print(head.val)
             if head.val != r_list.val:
-                r_list.next = head
-            
+                r_list.next = ListNode(head.val)
+                r_list = r_list.next
+           
             head = head.next
+            
         
-        # print(r_list)
-        while r_list !=None:
-            print(r_list.val)
-            r_list = r_list.next
-        
-        return r_list
+        return r_head
 
 if __name__ == "__main__":
 
@@ -33,11 +29,22 @@ if __name__ == "__main__":
     li = ListNode(1)
     li2 = ListNode(1)
     li3 = ListNode(2)
+    li35 = ListNode(2)
+    li4 = ListNode(3)
+    li5 = ListNode(3)
+    li6 = ListNode(3)
 
     li.next = li2
     li2.next = li3
+    li3.next = li35
+    li35.next = li4
+    li4.next = li5
+    li5.next = li6
 
-    sol.deleteDuplicates(li)
-            
+    rtv = sol.deleteDuplicates(li)
+    print('---------')
+    while rtv !=None:
+        print(rtv.val)
+        rtv = rtv.next
             
         
